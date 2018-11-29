fc942b779224cd36f69bfa052779a38dd205d86a
xinshasha <43017661+xinshasha@users.noreply.github.com>
2018-11-28 23:42:11 -0500

    Add files via upload

--- tweetAPI.py
+++ tweetAPI.py
@@ -15,8 +15,6 @@ def get_all_tweets(screen_name,pic_out):
     consumer_secret = "vKRaZcxfpBh"
     access_key = "725MwS"
     access_secret = "7qJh"
-
-
     #Twitter only allows access to a users most recent 3240 tweets with this method
     
     #authorize twitter, initialize tweepy
@@ -51,35 +49,15 @@ def get_all_tweets(screen_name,pic_out):
             break
         print("...%s tweets downloaded so far" % (len(alltweets)))
        
-    #write tweet objects to JSON
-    file = open('tweet.json', 'w') 
-    print ("Writing tweet objects to JSON please wait...")
+
     medias=[]
-
     m=0
-
     for status in alltweets:
-        #json.dump(status._json,file,sort_keys = True,indent = 4)
         tmp = status.entities.get('media', []);
-
-        #if(len(tmp)>0):
-        #	medias.append(tmp[0]['media_url'])
         if len(tmp)>0:
             urllib.request.urlretrieve(status.entities["media"][0]['media_url'],"./imgs/image%03d"%m)
             m=m+1
-            
-
-    name_index = 0
-
-    for i in medias:
-    	#wget.download(i,"./imgs")
-    	urllib.request.urlretrieve(i,pic_out+"img%03d.jpg"%name_index)
-    	name_index=name_index+1
-    	print (i+" is downloaded!")
-    
-    #close the file
     print("Done")
-    file.close()
 
 
 if __name__ == '__main__':
